52b91c9df8475550b5410c1cf1daee7b09e25e84
NugiInPrime <127527920+NugiInPrime@users.noreply.github.com>
2023-09-25 14:07:43 +0400

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly StoreContext Context;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            Context = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await Context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await Context.Products.FindAsync(id);
+        }
+    }
+}
